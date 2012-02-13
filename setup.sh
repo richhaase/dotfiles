c0903b166ca07ce1e35a5c8fea516d11d11bc11b
Rich Haase <rich.haase@level3.com>
2012-02-13 11:45:44 -0700

    fixed setup.sh... funny how easy it is to code when you aren't falling asleep.

--- setup.sh
+++ setup.sh
@@ -1,29 +1,21 @@
-#!/bin/bash                                                                                                                       
+#!/bin/bash
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATE=`date '+%y%m%d%H%M%S'`
 
-function link () {
-    echo $3
-    ln -s $1 $2
-}
-
-function rm_link () {
-    rm ~/.$1
-    link $DIR/$i ~/.$i "COMPLETE W/ REMOVED LINK"   
-}
-
-for i in "emacs" "emacs.d"; do
-    echo -n "SETTING UP .emacs: "
-    if [ -f ~/.$i ]; then
-	mv ~/.$i ~/.${i}_$DATE
-	link $DIR/$i ~/.$i "COMPLETE W/ BACKUP"
-    elif [ -d ~/.$i ]; then
-	rm_link ~/.$i
-    elif [ -L ~/.$i ]; then
-	rm_link ~/.$i
-    else
-	link $DIR/$i ~/.$i "COMPLETE"
+for i in `ls $DIR`; do
+    echo $i | egrep -v "#|~|setup.sh" > /dev/null 
+    if [ $? == 0 ]; then
+       if [ -L ~/.$i ]; then
+	   echo "REMOVING LINK: ~/.$i"
+	   rm ~/.$i
+       elif [ -e ~/.$i ]; then
+	   echo "SAVING ~/.$i as ~/.${i}_SAVE_$DATE"
+	   mv ~/.$i ~/.${i}_SAVE_$DATE
+       fi
+       echo "LINKING ~/.$i to $DIR/$i"
+       ln -s $DIR/$i ~/.$i
     fi
 done
+
 
