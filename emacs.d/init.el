33247dabd72eabc9a3b6613abcd38186d401c134
Rich Haase <rdhaase@gmail.com>
2013-02-17 14:37:10 -0700

    ""

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,19 @@
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 
+;; Clojure paredit hook
+(add-hook 'clojure-mode-hook
+	  (lambda ()
+	    (paredit-mode 1)
+	    ;; Get syntax highlighting in a REPL
+	    (add-hook 'slime-repl-mode-hook
+                      (defun clojure-mode-slime-font-lock ()
+                        (let (font-lock-mode)
+                          (clojure-mode-font-lock-setup))))))
+
+;; marmalade package repo
+(require 'package)
+(add-to-list 'package-archives
+	     '("maramalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
 
 
