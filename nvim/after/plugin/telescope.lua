9b47edbdd9c2ce83bb33a653390ba3d20e002723
Rich Haase <haaserdh@gmail.com>
2024-02-06 17:57:50 -0700

    Uses single characters to launch telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 local cd_picker = require("telescope.pickers").new({}, {
