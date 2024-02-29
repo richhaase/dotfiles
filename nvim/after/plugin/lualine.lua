5ec249bd3f3cd719fc8bfe1cf4f1bc9f820534cc
Rich Haase <haaserdh@gmail.com>
2024-02-28 17:06:31 -0700

    Adds a buffer line

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,13 @@
-require('lualine').setup {
+t aqqqqrequire('lualine').setup {
     options = {
         theme = 'wombat'
+    },
+    tabline = {
+        lualine_a = {'buffers'},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
     }
 }
