daacad9bbe39445b8cb96a1e34240cc9d1a21dd1
Rich Haase <haaserdh@gmail.com>
2025-06-19 10:04:15 -0600

    Update lazyvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -21,9 +21,9 @@ local opts = { noremap = true, silent = false }
 -- Create a new note after asking for its title.
 vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
 -- Create or open daily note
-vim.api.nvim_set_keymap("n", "<leader>zd", "<Cmd>ZkNew { dir = 'daily' }<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>zd", "<Cmd>ZkNew { dir = 'journal/daily' }<CR>", opts)
 -- Create or open yesterdays daily note
-vim.api.nvim_set_keymap("n", "<leader>zy", "<Cmd>ZkNew { dir = 'daily', date = 'yesterday' }<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>zy", "<Cmd>ZkNew { dir = 'journal/daily', date = 'yesterday' }<CR>", opts)
 -- Open notes.
 vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
 -- Open notes associated with the selected tags.
