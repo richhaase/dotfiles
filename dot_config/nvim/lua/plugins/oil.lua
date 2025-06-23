3644e1003e238e31c217639137f868219fd69c7f
Rich Haase <haaserdh@gmail.com>
2025-06-23 17:30:14 -0600

    Removes oil and vim-sleuth

--- dot_config/nvim/lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opts = {
-    default_file_explorer = false,  -- Keep netrw as default
-  },
-  keys = {
-    { "<leader>o", "<cmd>Oil<cr>", desc = "Open Oil file manager" },
-  },
-}
