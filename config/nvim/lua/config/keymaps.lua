c6cfc27d61cb1d9f1b9345a28b11b911d8eb814e
Rich Haase <haaserdh@gmail.com>
2025-07-12 19:15:01 -0600

    Initial commit of porting dotfiles to plonk!

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Quick shell command
+vim.keymap.set("n", "<leader>!", ":!", { desc = "Run shell command" })
