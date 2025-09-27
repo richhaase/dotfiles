deaaf6dc954660216d27f71de5543bb577611e47
Rich Haase <haaserdh@gmail.com>
2025-09-26 18:26:08 -0600

    zsh: add Helix grammar helpers
    - hxg: fetch + build grammars in one shot
    - hxgh: grammar health check

--- config/zsh/lib/tools.sh
+++ config/zsh/lib/tools.sh
@@ -6,3 +6,14 @@
 alias lg="lazygit"
 alias nv='nvim'
 alias dps='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"'
+
+# Helix helpers
+hxg() {
+  if ! (( $+commands[hx] )); then
+    echo "hxg: helix (hx) not found in PATH" >&2
+    return 127
+  fi
+  hx --grammar fetch && hx --grammar build
+}
+
+alias hxgh='hx --grammar health'
