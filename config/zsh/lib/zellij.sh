20d3b533973b87b7975c26117dbd259f3294c3da
Rich Haase <46830998+richhaase@users.noreply.github.com>
2025-12-29 16:29:08 -0700

    remove unused functions/aliases

--- config/zsh/lib/zellij.sh
+++ config/zsh/lib/zellij.sh
@@ -2,9 +2,6 @@
 # Zellij Terminal Multiplexer
 # ============================================================================
 
-alias zj='zellij'
-alias zjw='zellij -l welcome'
-
 # Zellij floating run command
 zrf() {
   if [ $# -eq 0 ]; then
@@ -24,16 +21,6 @@ zr() {
   else
     zellij run --name "$*" -- zsh -ic "$*"
   fi
-}
-
-# Zellij floating edit command
-zef() {
-  zellij edit --floating -x 10% -y 10% --width 80% --height 80% "$1";
-}
-
-# Zellij floating edit command
-ze() {
-  zellij edit "$1";
 }
 
 # Open a new Zellij tab in a target directory (or CWD) with optional layout
@@ -60,24 +47,6 @@ zt() {
   # Resolve absolute path and replace $HOME/ with nothing (relative path from home)
   target_path=${target_path:A}
   name="${target_path/#$HOME\//}"
-
-  # If we're outside Zellij, start or attach to a session rooted in the target dir
-  if [ -z "${ZELLIJ:-}" ]; then
-    if ! (
-      builtin cd "$target_path" || exit 1
-      if [ -n "$layout" ]; then
-        zellij --new-session-with-layout "$layout" --session "$name" ||
-          zellij attach --create "$name"
-      else
-        zellij --session "$name" ||
-          zellij attach --create "$name"
-      fi
-    ); then
-      printf 'zt: failed to start zellij session\n' >&2
-      return 1
-    fi
-    return 0
-  fi
 
   # Open new tab in the target directory
   if [ -n "$layout" ]; then
