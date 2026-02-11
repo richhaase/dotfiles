21f5ac3ced42ebb783ee923a2aaf5e3a14379035
Rich Haase <46830998+richhaase@users.noreply.github.com>
2026-02-11 11:25:58 -0700

    Remove all zellij shell helpers and aliases

--- config/zsh/lib/zellij.sh
+++ config/zsh/lib/zellij.sh
@@ -1,15 +1,3 @@
 # ============================================================================
 # Zellij Terminal Multiplexer
 # ============================================================================
-
-# Zellij floating run command
-zrf() {
-  if [ $# -eq 0 ]; then
-    zellij run --floating -x 10% -y 10% --width 80% --height 80% -- zsh
-  else
-    zellij run --name "$*" --floating -x 10% -y 10% --width 80% --height 80% -- zsh -ic "$*"
-  fi
-}
-
-alias zlg='zrf lg'
-alias zbt='zrf btop'
